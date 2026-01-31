b587c68d5e01be4e81394f14b7930267abe67fad
rodrigosistemas <rodhernandez.code@gmail.com>
2026-01-30 20:06:44 -0500

    Redireccion de pag no encontrada en Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './views/Login';
 import Portal from './views/Portal';
 
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/portal" element={<Portal />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
